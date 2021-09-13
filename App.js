07dc403cd407b61868c2420fd89f35c286d73757
Denner <morgan.espindola@gmail.com>
2021-09-13 08:04:52 -0300

    Componentization

--- App.js
+++ App.js
@@ -1,39 +1,6 @@
-import React, { useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+import Main from './src';
 
-const App = () => {
-
-  const [like, setLike] = useState(0);
-
-  const incrementLike = () => {
-    setLike(like + 1);
-  }
-
-  const handleLikeButton = () => {
-    incrementLike();
-  }
-
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Likes: {like}</Text>
-      <TouchableOpacity onPress={handleLikeButton}>
-        <Text>Like</Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  text: {
-    fontSize: 22,
-    color: '#00f'
-  },
-})
+const App = () => <Main />
 
 export default App;
